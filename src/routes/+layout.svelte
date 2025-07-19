62a4357255061bf7f9514b212a1e282545677650
harvanchik <jh6@harvhouse.com>
2025-07-19 15:31:02 -0700

    added navigation progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,20 @@
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
 	import type { LayoutProps } from './$types';
+	import { navigating } from '$app/stores';
+	import NProgress from 'nprogress';
+	import 'nprogress/nprogress.css';
 	let { children, data }: LayoutProps = $props();
+
+	NProgress.configure({ minimum: 0.16, showSpinner: false });
+
+	$effect(() => {
+		if ($navigating) {
+			NProgress.start();
+		} else {
+			NProgress.done();
+		}
+	});
 </script>
 
 {#if data.user}
